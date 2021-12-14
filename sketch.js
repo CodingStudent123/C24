bd49e43e0093057a2ea947c4afbd6af27e402a2f
CodingStudent123 <92556990+CodingStudent123@users.noreply.github.com>
2021-12-13 16:41:03 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+var balls = [];
+
+//examples of an array
+var arr = [1,2,3];
+console.log(arr);
+
+//arrays containing different data types
+var arr1 = ["ashraful", 13, "avtgt@gmail.com", 4206942069, true];
+console.log(arr1);
+
+//arrays containing list of arrays
+var arr2 = [[1,2], [3,4], [5,6]];
+console.log(arr2);
+console.log(arr2[0]);
+console.log(arr2[0][1]);
+
+//adding information in an array
+arr1.push("8th grade");
+console.log(arr1);
+
+//taking out information from an array
+arr1.pop();
+console.log(arr1);
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+  cannon = new Cannon(180, 110, 130, 100, angle);
+  cannonBall = new CannonBall(cannon.x, cannon.y);
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+
+  push();
+  fill("brown");
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  for (var i = 0; i<balls.length; i++) {
+    showCannonBalls(balls[i]);
+  }
+
+  cannon.display();
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall);
+  }
+}
+
+function showCannonBalls(balls, i) {
+  if (balls) {
+    balls.display();
+  }
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length-1].shoot();
+  }
+}
